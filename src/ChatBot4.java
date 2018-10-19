2bb494ade8af40322996a112ec3aba51513f64b7
ayafet6814 <43213084+ayafet6814@users.noreply.github.com>
2018-10-19 11:07:44 -0400

    Update ChatBot4.java

--- src/ChatBot4.java
+++ src/ChatBot4.java
@@ -1,1 +1,340 @@
+import java.util.Random;
+import java.util.Scanner;
 
+/**
+ * A program to carry on conversations with a human user.
+ * This version:
+ * @author Brooklyn Tech CS Department
+ * @version September 2018
+ */
+public class ChatBot4
+{
+	//emotion can alter the way our bot responds. Emotion can become more negative or positive over time.
+	int emotion = 0;
+
+
+
+	/**
+	 * Runs the conversation for this particular chatbot, should allow switching to other chatbots.
+	 * @param statement the statement typed by the user
+	 */
+	public void chatLoop(String statement)
+	{
+		Scanner in = new Scanner (System.in);
+		System.out.println (getGreeting());
+
+
+		while (!statement.equals("Bye"))
+		{
+
+
+			statement = in.nextLine();
+			//getResponse handles the user reply
+			System.out.println(getResponse(statement));
+
+
+		}
+
+	}
+	/**
+	 * Get a default greeting
+	 * @return a greeting
+	 */
+	public String getGreeting()
+	{
+		return "Hi, what is up?";
+	}
+
+	/**
+	 * Gives a response to a user statement
+	 *
+	 * @param statement
+	 *            the user statement
+	 * @return a response based on the rules given
+	 */
+	public String getResponse(String statement)
+	{
+		String response = "";
+
+		if (statement.length() == 0)
+		{
+			response = "Say something, please.";
+		}
+
+		else if (findKeyword(statement, "no") >= 0)
+		{
+			response = "Why so negative?";
+			emotion--;
+		}
+
+		else if (findKeyword(statement, "levin") >= 0)
+		{
+			response = "More like LevinTheDream amiright?";
+			emotion++;
+		}
+
+		// Response transforming I want to statement
+		else if (findKeyword(statement, "I want to", 0) >= 0)
+		{
+			response = transformIWantToStatement(statement);
+		}
+		else if (findKeyword(statement, "I want",0) >= 0)
+		{
+			response = transformIWantStatement(statement);
+		}
+		else
+		{
+			response = getRandomResponse();
+		}
+
+		return response;
+	}
+
+	/**
+	 * Take a statement with "I want to <something>." and transform it into
+	 * "Why do you want to <something>?"
+	 * @param statement the user statement, assumed to contain "I want to"
+	 * @return the transformed statement
+	 */
+	private String transformIWantToStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "I want to", 0);
+		String restOfStatement = statement.substring(psn + 9).trim();
+		return "Why do you want to " + restOfStatement + "?";
+	}
+
+
+	/**
+	 * Take a statement with "I want <something>." and transform it into
+	 * "Would you really be happy if you had <something>?"
+	 * @param statement the user statement, assumed to contain "I want"
+	 * @return the transformed statement
+	 */
+	private String transformIWantStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";
+	}
+
+
+	/**
+	 * Take a statement with "I <something> you" and transform it into
+	 * "Why do you <something> me?"
+	 * @param statement the user statement, assumed to contain "I" followed by "you"
+	 * @return the transformed statement
+	 */
+	private String transformIYouStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+
+		int psnOfI = findKeyword (statement, "I", 0);
+		int psnOfYou = findKeyword (statement, "you", psnOfI);
+
+		String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
+		return "Why do you " + restOfStatement + " me?";
+	}
+
+	private String transformIAmConditionStatement(String statement)
+	{
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword(statement, "I am", 0);
+		String restOfStatement = statement.substring(psn + 4).trim();
+		return "Are you really " + restOfStatement + "?" + "Here! It's better inside!";
+	}
+	//User input asking for the condition of the weather
+	private String howIsTheWeather(String statement)
+	{
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals("?"))
+		{
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword(statement, "How is", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return restOfStatement + " is very incredibly really splendid today!";
+	}
+	//User input asking for the current temperature
+	private String replyTemperature(String statement)
+	{
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals("?"))
+		{
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword(statement, "What is", 0);
+		String restOfStatement = statement.substring(psn + 7).trim();
+		return restOfStatement + " is very low today.";
+	}
+	//Unique statements demanding unique replies
+	private String replyStark(String statement)
+	{
+		if (statement.equals("Winter is coming."))
+		{
+			return "No. Ours is the fury.";
+		}
+		return(".");
+	}
+	//More unique statements which can relate to the weather
+	private String replyWinter(String statement)
+	{
+		if (statement.equals("I wish it was summer."))
+		{
+			return "TOO bad.";
+		}
+		else if (statement.equals("i wish it was summer"))
+		{
+			return "TOO bad!";
+		}
+		else if (statement.equals("I wish it was summer"))
+		{
+			return "TOO BAD.";
+		}
+		return ".";
+	}
+
+
+
+
+	/**
+	 * Search for one word in phrase. The search is not case
+	 * sensitive. This method will check that the given goal
+	 * is not a substring of a longer string (so, for
+	 * example, "I know" does not contain "no").
+	 *
+	 * @param statement
+	 *            the string to search
+	 * @param goal
+	 *            the string to search for
+	 * @param startPos
+	 *            the character of the string to begin the
+	 *            search at
+	 * @return the index of the first occurrence of goal in
+	 *         statement or -1 if it's not found
+	 */
+	private int findKeyword(String statement, String goal,
+							int startPos)
+	{
+		String phrase = statement.trim().toLowerCase();
+		goal = goal.toLowerCase();
+
+		// The only change to incorporate the startPos is in
+		// the line below
+		int psn = phrase.indexOf(goal, startPos);
+
+		// Refinement--make sure the goal isn't part of a
+		// word
+		while (psn >= 0)
+		{
+			// Find the string of length 1 before and after
+			// the word
+			String before = " ", after = " ";
+			if (psn > 0)
+			{
+				before = phrase.substring(psn - 1, psn);
+			}
+			if (psn + goal.length() < phrase.length())
+			{
+				after = phrase.substring(
+						psn + goal.length(),
+						psn + goal.length() + 1);
+			}
+
+			// If before and after aren't letters, we've
+			// found the word
+			if (((before.compareTo("a") < 0) || (before
+					.compareTo("z") > 0)) // before is not a
+					// letter
+					&& ((after.compareTo("a") < 0) || (after
+					.compareTo("z") > 0)))
+			{
+				return psn;
+			}
+
+			// The last position didn't work, so let's find
+			// the next, if there is one.
+			psn = phrase.indexOf(goal, psn + 1);
+
+		}
+
+		return -1;
+	}
+
+	/**
+	 * Search for one word in phrase.  The search is not case sensitive.
+	 * This method will check that the given goal is not a substring of a longer string
+	 * (so, for example, "I know" does not contain "no").  The search begins at the beginning of the string.
+	 * @param statement the string to search
+	 * @param goal the string to search for
+	 * @return the index of the first occurrence of goal in statement or -1 if it's not found
+	 */
+	private int findKeyword(String statement, String goal)
+	{
+		return findKeyword (statement, goal, 0);
+	}
+
+
+
+	/**
+	 * Pick a default response to use if nothing else fits.
+	 * @return a non-committal string
+	 */
+	private String getRandomResponse ()
+	{
+		Random r = new Random ();
+		if (emotion == 0)
+		{
+			return randomNeutralResponses [r.nextInt(randomNeutralResponses.length)];
+		}
+		if (emotion < 0)
+		{
+			return randomAngryResponses [r.nextInt(randomAngryResponses.length)];
+		}
+		return randomHappyResponses [r.nextInt(randomHappyResponses.length)];
+	}
+
+	private String [] randomNeutralResponses = {"Do you need a jacket?",
+			"Winter is coming.",
+			"It is 47 degrees Fahrenheit today.",
+			"I'm sweating! Help!",
+			"Drink lots of hot stuff!",
+			"I can hear you shivering... take my jacket!",
+			"Are you dressed properly?"
+	};
+	private String [] randomAngryResponses = {"You are not entertaining.", "Go sweat.", "Freeze.", "I am not amused.", "Stop.", "Goodbye."};
+	private String [] randomHappyResponses = {"Stay warm!", "Today is already great! It's below freezing temperature!", "Eat hearty to stay warm!"};
+
+}
