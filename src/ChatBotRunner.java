a106dd031158ddfe9445c4dd7e277a69262ec39a
jwu8483 <43213089+jwu8483@users.noreply.github.com>
2018-10-19 11:07:18 -0400

    Update ChatBotRunner.java

--- src/ChatBotRunner.java
+++ src/ChatBotRunner.java
@@ -16,11 +16,11 @@ import java.util.Scanner;
 				ChatBot1 chatbot1 = new ChatBot1();
 				ChatBot2 chatbot2 = new ChatBot2();
 				ChatBot3 chatbot3 = new ChatBot3();
-				//ChatBot4 chatbot4 = new ChatBot4();
+				ChatBot4 chatbot4 = new ChatBot4();
 
 
 				Scanner in = new Scanner (System.in);
-				System.out.println("Good Morning it is 6 o'clock. What would you like to talk about? (1)Weather (2)Music (3)Breakfast (4)Clothing");
+				System.out.println("Good Morning it is 6 o'clock. What would you like to talk about? (1)Clothing (2)Music (3)Breakfast (4)Weather");
 				int statement = in.nextInt();
 				if (statement == 1)
 				{
@@ -30,14 +30,18 @@ import java.util.Scanner;
 				{
 					chatbot2.chatLoop("1");
 				}
-				else
+				else if(statement == 3)
 				{
 					chatbot3.chatLoop("1");
 				}
-				//else
-				//{
-					//chatbot4.chatLoop("4");
-				//}
+				else if(statement == 4)
+				{
+					chatbot4.chatLoop("1");
+				}
+				else
+				{
+					System.out.println("Please enter a valid number");
+				}
 
 
 
